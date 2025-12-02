acc4c54fcc2015622a61154f7570be47c3e4eedc
RubeBonilla <bonillamanzano991102@gmail.com>
2025-12-02 17:31:31 -0600

    Termino de / mostrar Contacto / buscar Cotacto / actualizar Contacto

--- src/gestor/ContactoService.java
+++ src/gestor/ContactoService.java
@@ -19,10 +19,34 @@ public class ContactoService {
 
     public void mostrarContactos(){
         for(Contacto c : contacto){
-
+            System.out.println("ID: " + c.getId() + " | Nombre: " + c.getNombre()
+            + " | Telefono: " + c.getNumero());
         }
     }
 
+    public Contacto  buscarContactoPorId(int id){
+        for (Contacto c : contacto){
+            if (c.getId() == id){
+                return c;
+            }
+        }
+
+        return null;
+
+
+    }
+
+    public void actualizarNumero(int id, long numeroNuevo){
+        for (Contacto c : contacto){
+            if (c.getId() == id){
+                c.setNumero(numeroNuevo);
+                System.out.println("Numero actualizado correctamente!!!");
+                return;
+            }
+        }
+        System.out.println("No se encontro el ID que busca :(");
+
+    }
 
 
 
