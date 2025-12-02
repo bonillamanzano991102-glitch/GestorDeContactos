22878d0df9d4b1b378463140a51cc2c3ac3c9671
RubeBonilla <bonillamanzano991102@gmail.com>
2025-12-02 00:19:42 -0600

    Creacion del metodo agregarContacto para guardarlo en la lista

--- src/gestor/ContactoService.java
+++ src/gestor/ContactoService.java
@@ -1,3 +1,29 @@
+package gestor;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContactoService {
-    
+
+    private List<Contacto> contacto = new ArrayList<>();
+    private int contador = 1;
+
+    public void agregarContacto(String nombre, long numero){
+        int id = contador;
+
+        Contacto nuevoContacto = new Contacto(id, nombre, numero);
+        contacto.add(nuevoContacto);
+
+        contador ++;
+    }
+
+    public void mostrarContactos(){
+        for(Contacto c : contacto){
+
+        }
+    }
+
+
+
+
 }
