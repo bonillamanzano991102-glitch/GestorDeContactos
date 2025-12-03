d34737a1eeed70137fa3014d39a101234ced3f82
RubeBonilla <bonillamanzano991102@gmail.com>
2025-12-02 18:12:03 -0600

    Agregando ContactoApp

---
+++ src/gestor/ContactoApp.java
@@ -0,0 +1,31 @@
+package gestor;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class ContactoApp {
+    public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    ContactoService contactoService = new ContactoService();
+
+    int opcion = 0;
+
+    while (opcion != 5){
+        System.out.println("¿QUE DECEAS HACER? ELIJE ALGUNA DE LAS 5 OPCIONES:");
+        System.out.println("1.- Agregar Contacto:");
+        System.out.println("2.- Mostrar Contactos:");
+        System.out.println("3.- Buscar Contacto Por ID:");
+        System.out.println("4.- Editar numero de Telefono:");
+        System.out.println("5.- Salir");
+
+        switch (opcion){
+            
+        }
+
+
+    }
+
+
+    }
+}
+
